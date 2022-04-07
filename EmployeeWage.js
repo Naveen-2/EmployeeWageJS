1c3203d49586094deb04b871c819582d4d5547e5
Naveen-2 <naveen2k15cse@gmail.com>
2022-04-07 20:16:13 +0530

    Calculated Wages for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,14 +5,17 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUMBER_OF_WORKING_DAYS = 20;
 
 let empHrs = 0;
 
-empCheck = Math.floor(Math.random() * 10) % 3;
+for (let day = 0; day < NUMBER_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
 
-empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage: " + empWage);
+console.log(`Total Hours: ${empHrs}\nEmp Wage: ${empWage}`);
 
 
 function getWorkingHours(empCheck){
