c7ef4e4acf490bdc9a56b0b9af3c9f5a71ffa952
Naveen-2 <naveen2k15cse@gmail.com>
2022-04-07 20:39:47 +0530

    Employee Wage calculated till Maximum working days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,18 +5,23 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+
 const NUMBER_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 100;
 
-let empHrs = 0;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
 
-for (let day = 0; day < NUMBER_OF_WORKING_DAYS; day++){
+while(totalEmpHrs <= MAX_HRS_IN_MONTH &&
+      totalWorkingDays < NUMBER_OF_WORKING_DAYS) {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
 
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log(`Total Hours: ${empHrs}\nEmp Wage: ${empWage}`);
-
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days: " + totalWorkingDays +
+                "\nTotal Hrs: " + totalEmpHrs + "\nEmp Wage: " + empWage);
 
 function getWorkingHours(empCheck){
     switch(empCheck){
